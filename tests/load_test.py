00e424d641a4d3bc69ac0ecdcaf77771751c26a2
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-07-05 17:52:47 -0500

    Implemented locust for load test

---
+++ tests/load_test.py
@@ -0,0 +1,21 @@
+from locust import HttpUser, TaskSet, task, between
+
+"""
+Run locus with:
+locust -f ./tests/load_test.py
+"""
+
+
+class IrisPredict(TaskSet):
+    @task
+    def predict(self):
+        request_body = {"data": [[4.8, 3, 1.4, 0.3]]}
+        self.client.post('/predict', json=request_body)
+
+
+
+class IrisLoadTest(HttpUser):
+    tasks = [IrisPredict]
+    host = 'http://127.0.0.1'
+    stop_timeout = 20
+    wait_time = between(1, 5)
